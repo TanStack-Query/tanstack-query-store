9291f734c50fae941a85bc87a8ac8466458ff7c7
= <=>
2024-08-13 13:08:37 +0200

    Optimistic updates, Success and Error

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -6,18 +6,68 @@ export const useProductMutation = () => {
 
   const mutation = useMutation({
     mutationFn: productActions.createProduct,
-    onSuccess: (data) => {
+
+    // Optimistic update
+    onMutate: (data) => {
+      const optimisticProduct = { id: Math.random(), ...data };
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: data.category }],
+        (old) => {
+          if (!old) return [optimisticProduct];
+          return [...old, optimisticProduct];
+        }
+      );
+      return { optimisticProduct };
+    },
+
+    onSuccess: (data, variables, context) => {
       //   Invalidate query
       //   queryClient.invalidateQueries({
       //     queryKey: ["products", { filterKey: data.category }],
       //   });
 
       // Not invalidate query
+      //   queryClient.setQueryData<Product[]>(
+      //     ["products", { filterKey: data.category }],
+      //     (old) => {
+      //       if (!old) return [data];
+      //       return [...old, data];
+      //     }
+      //   );
+
+      // Optimistic update
+      variables = variables!;
+      queryClient.removeQueries({
+        queryKey: ["product", context?.optimisticProduct.id],
+      });
       queryClient.setQueryData<Product[]>(
         ["products", { filterKey: data.category }],
         (old) => {
           if (!old) return [data];
-          return [...old, data];
+
+          return old.map((cacheProduct) => {
+            return cacheProduct.id === context?.optimisticProduct.id
+              ? data
+              : cacheProduct;
+          });
+        }
+      );
+    },
+
+    onError: (error, variables, context) => {
+      variables = variables!;
+      queryClient.removeQueries({
+        queryKey: ["product", context?.optimisticProduct.id],
+      });
+
+      queryClient.setQueryData<Product[]>(
+        ["products", { filterKey: variables.category }],
+        (old) => {
+          if (!old) return [];
+
+          return old.filter((cacheProduct) => {
+            return cacheProduct.id !== context?.optimisticProduct.id;
+          });
         }
       );
     },
